175c57e24192923cf74cdf77499e20ec8bd07425
Steven <pslopez91@soy.sena.edu.co>
2024-07-24 16:28:09 -0500

    entidad creada

---
+++ src/main/java/com/api/java/api_products_2826505/ApiProducts2826505Application.java
@@ -0,0 +1,13 @@
+package com.api.java.api_products_2826505;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProducts2826505Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProducts2826505Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/java/api_products_2826505/ApiProducts2826505ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.java.api_products_2826505;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProducts2826505ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
