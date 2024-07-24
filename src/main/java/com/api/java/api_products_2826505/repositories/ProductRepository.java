d46dfbc669b87f64dea4bde3b5e39e8515f1785c
Steven <pslopez91@soy.sena.edu.co>
2024-07-24 17:21:10 -0500

    repositorio creado

---
+++ src/main/java/com/api/java/api_products_2826505/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.api.java.api_products_2826505.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.api.java.api_products_2826505.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product , Long>{
+        //Repositorio por cada entidad
+        
+    
+} 
